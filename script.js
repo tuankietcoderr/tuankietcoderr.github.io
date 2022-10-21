540282f9ae1a0695f254418584ef2062b11ec069
tuankietcoder <charlieismylife2003@gmail.com>
2022-10-22 00:21:16 +0700

    Add animation and responsive

--- script.js
+++ script.js
@@ -1,8 +1,31 @@
-const imgList = document.querySelectorAll("img");
-// console.log(imgList);
+document
+  .querySelectorAll(".animate")
+  .forEach((item, index) =>
+    setTimeout(() => item.classList.add("active"), index * 1000)
+  );
+function reveal() {
+  const reveals = document.querySelectorAll(".reveal");
+  const reveals_more = document.querySelectorAll(".reveal_more");
+  for (let i = 0; i < reveals_more.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals_more[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals_more[i].classList.add("active");
+    } else {
+      reveals_more[i].classList.remove("active");
+    }
+  }
+  for (let i = 0; i < reveals.length; i++) {
+    const windowHeight = window.innerHeight;
+    const elementTop = reveals[i].getBoundingClientRect().top;
+    const elementVisible = 150;
+    if (elementTop < windowHeight - elementVisible) {
+      reveals[i].classList.add("active");
+    } else {
+      reveals[i].classList.remove("active");
+    }
+  }
+}
 
-imgList.forEach((img) => {
-  const { baseURI, src } = img;
-  const { pathname } = location;
-  console.log(pathname);
-});
+window.addEventListener("scroll", reveal, { capture: true });
